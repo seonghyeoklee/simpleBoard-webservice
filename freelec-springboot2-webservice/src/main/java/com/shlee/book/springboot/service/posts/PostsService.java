36de7db98bdaef2ff585d85250a668fac6552bf4
megamax23 <megamax23@gmail.com>
2020-01-22 18:13:05 +0900

    posts api 수정

--- freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/service/posts/PostsService.java
+++ freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.shlee.book.springboot.service.posts;
 
 import com.shlee.book.springboot.domain.posts.Posts;
 import com.shlee.book.springboot.domain.posts.PostsRepository;
+import com.shlee.book.springboot.web.dto.PostsListResponseDto;
 import com.shlee.book.springboot.web.dto.PostsResponseDto;
 import com.shlee.book.springboot.web.dto.PostsSaveRequestDto;
 import com.shlee.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +40,19 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
 
 }
--- freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/web/IndexController.java
@@ -1,13 +1,31 @@
 package com.shlee.book.springboot.web;
 
+import com.shlee.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(Model model, @PathVariable Long id) {
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
     }
 }
