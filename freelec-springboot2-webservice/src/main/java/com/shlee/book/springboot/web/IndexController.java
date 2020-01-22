7bec4ff58b4c527fa0e9dbccd23a3cb7b8c99e5a
Seong Hyeok Lee <dltjdgur327@gmail.com>
2020-01-23 00:23:11 +0900

    spring security google

--- freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.shlee.book.springboot.web;
 
+import com.shlee.book.springboot.config.auth.dto.SessionUser;
+import com.shlee.book.springboot.domain.user.User;
 import com.shlee.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,14 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
