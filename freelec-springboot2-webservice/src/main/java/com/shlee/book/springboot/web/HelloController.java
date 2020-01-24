e04f56181a9c9d0ea0fd1380f31e5c0be0841d18
Seong Hyeok Lee <dltjdgur327@gmail.com>
2020-01-24 23:52:58 +0900

    init project

--- freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/web/HelloController.java
+++ freelec-springboot2-webservice/src/main/java/com/shlee/book/springboot/web/HelloController.java
@@ -1,11 +1,13 @@
 package com.shlee.book.springboot.web;
 
 import com.shlee.book.springboot.web.dto.HelloResponseDto;
+import lombok.extern.log4j.Log4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
